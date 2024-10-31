2325a194fd9cadcb41ee9824de2a0213a0e8fa57
Eden Gaon <edencgaon@gmail.com>
2024-10-31 11:29:23 +0200

    Move between displays

--- .install.sh
+++ .install.sh
@@ -3,6 +3,11 @@
 ## Install
 echo "Installing Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrew to PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew analytics off
 
 ## Taps
